74be56333b9790af72830beeeb97981e99f2abd3
Budimir Budimir <budimir.budimir@Budimirs-MacBook-Pro.local>
2021-04-05 23:46:58 +0200

    minor one

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,15 +3,15 @@ import { AnchorLink } from "gatsby-plugin-anchor-links"
 // import content from "../../content/hero.yaml"
 
 // import { IntlProvider, useIntl } from "../hooks/useIntl"
-import contentEN from "../../content/en/hero.yaml"
-import contentDE from "../../content/de/hero.yaml"
-import contentAL from "../../content/al/hero.yaml"
-// import getContent from "../hooks/getContent"
+// import contentEN from "../../content/en/hero.yaml"
+// import contentDE from "../../content/de/hero.yaml"
+// import contentAL from "../../content/al/hero.yaml"
+import getContent from "../hooks/getContent"
 
 export default ({ lang }) => {
-  // const content = getContent("hero", lang)
-  const map = { en: contentEN, de: contentDE, al: contentAL }
-  const content = map[lang]
+  const content = getContent("hero", lang)
+  // const map = { en: contentEN, de: contentDE, al: contentAL }
+  // const content = map[lang]
 
   return (
     <section id="hero" style={{ paddingTop: "27px" }}>
@@ -20,23 +20,27 @@ export default ({ lang }) => {
         {/* <div className="twelve columns"> */}
         <div>
           <div className="hero-text">
-            <h1 className="responsive-headline">{content.headline}</h1>
-            <p>{content.body}</p>
+            <h1 className="responsive-headline">{content.headline || ""}</h1>
+            <p>{content.body || ""}</p>
           </div>
 
           <div className="hero-image">
-            <img src={content.image} alt="" className="animated fadeInUpBig" />
+            <img
+              src={content.image || ""}
+              alt=""
+              className="animated fadeInUpBig"
+            />
           </div>
 
           <div className="buttons" style={{ paddingTop: "40px" }}>
             <AnchorLink
               className="button trial animated shake"
-              to={content.button1.to}
+              to={content.button1.to || ""}
             >
-              {content.button1.label}
+              {content.button1.label || ""}
             </AnchorLink>
             <AnchorLink className="button learn-more" to={content.button2.to}>
-              {content.button2.label}
+              {content.button2.label || ""}
             </AnchorLink>
           </div>
         </div>
